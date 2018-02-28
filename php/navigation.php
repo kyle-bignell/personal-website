674c85158fb1e1e399b2329650acf0de1444ffb5
Kyle Bignell <Kyle.Bignell@gmail.com>
2018-02-28 01:20:57 +0000

    Added structure for professional experience

--- php/navigation.php
+++ php/navigation.php
@@ -5,17 +5,17 @@
     $page = basename($_SERVER['PHP_SELF']);
 
     $indexActiveClass = ($page == "index.php") ? "active-navigation-node" : "inactive-navigation-node";
-    $professionalProjectsActiveClass = ($page == "professional_projects.php") ? "active-navigation-node" : "inactive-navigation-node";
+    $professionalExperienceActiveClass = ($page == "professional_experience.php") ? "active-navigation-node" : "inactive-navigation-node";
     $sideProjectsActiveClass = ($page == "side_projects.php") ? "active-navigation-node" : "inactive-navigation-node";
     $cvActiveClass = ($page == "cv.php") ? "active-navigation-node" : "inactive-navigation-node";
     $contactActiveClass = ($page == "contact.php") ? "active-navigation-node" : "inactive-navigation-node";
 
     echo '
-        <div id="navigation" class="navigation-bottom">
+        <div id="navigation">
             <div id="navigation-about" class="navigation-node ' . $indexActiveClass . '">
                 <a class="navigation-link" href="index.php">Home</a>
             </div>
-            <div id="navigation-projects" class="navigation-node ' . $professionalProjectsActiveClass . '">
+            <div id="navigation-projects" class="navigation-node ' . $professionalExperienceActiveClass . '">
                 <a class="navigation-link" href="professional_experience.php">Professional Experience</a>
             </div>
             <div id="navigation-projects" class="navigation-node ' . $sideProjectsActiveClass . '">
--- professional_experience.php
+++ professional_experience.php
@@ -21,10 +21,32 @@
 
     <div id="content">
         <div id="experience-menu">
-            <ul>
-                <li>Red7 - Senior Software Engineer</li>
-                <li>Red7 - Software Engineer</li>
-                <li>CCFE - Junior Software Engineer</li>
+            <ul class="experience-menu-parent-list">
+                <li>
+                    <div class="experience-menu-company">Senior Software Engineer</div>
+                    Red7
+                    <ul class="experience-menu-child-list">
+                        <li>- Responsibilities</li>
+                        <li>- Framework</li>
+                        <li>- Continuous Deployment</li>
+                        <li>- Skype Bot</li>
+                        <li class="experience-menu-child-list-bottom">- Tools</li>
+                    </ul>
+                </li>
+                <li>
+                    <div class="experience-menu-company">Software Engineer</div>
+                    Red7
+                    <ul class="experience-menu-child-list">
+                        <li class="experience-menu-child-list-bottom">- Responsibilities</li>
+                    </ul>
+                </li>
+                <li>
+                    <div class="experience-menu-company">Junior Software Engineer</div>
+                    CCFE
+                    <ul class="experience-menu-child-list">
+                        <li class="experience-menu-child-list-bottom">- Responsibilities</li>
+                    </ul>
+                </li>
             </ul>
         </div>
         <div id="experience-data">
@@ -40,7 +62,7 @@
                 </div>
                 <div class="experience-subsection">
                     <div class="experience-subsection-title">
-                        Role Descrption
+                        Responsibilities
                     </div>
                 </div>
                 <div class="experience-subsection">
@@ -61,6 +83,44 @@
                 <div class="experience-subsection">
                     <div class="experience-subsection-title">
                         Tools
+                    </div>
+                </div>
+            </div>
+
+            <hr class="experience-separator"/>
+
+            <div class="experience-section">
+                <div class="experience-section-title">
+                    Software Engineer
+                </div>
+                <div class="experience-section-time">
+                    May 2015 - Dec 2016
+                </div>
+                <div class="experience-section-company">
+                    Red7 Mobile (a part of Scientific Games Corporation)
+                </div>
+                <div class="experience-subsection">
+                    <div class="experience-subsection-title">
+                        Responsibilities
+                    </div>
+                </div>
+            </div>
+
+            <hr class="experience-separator"/>
+
+            <div class="experience-section">
+                <div class="experience-section-title">
+                    Software Engineer
+                </div>
+                <div class="experience-section-time">
+                    Jul 2013 - Jun 2014
+                </div>
+                <div class="experience-section-company">
+                    Culham Centre for Fusion Energy
+                </div>
+                <div class="experience-subsection">
+                    <div class="experience-subsection-title">
+                        Responsibilities
                     </div>
                 </div>
             </div>
