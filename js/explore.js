7b5aa7ba1435b7a54d5402832bd321ffd6dd442a
Kyle Bignell <Kyle.Bignell@gmail.com>
2019-01-14 23:30:48 +0000

    Updated orbit scene

--- js/explore.js
+++ js/explore.js
@@ -59,10 +59,6 @@ function loadExplore()
     document.getElementsByTagName('head')[0].appendChild(phaserScript);
 }
 
-var rocket;
-var particlesEmitterLeft;
-var particlesEmitterRight;
-
 function setupExplore()
 {
     var SceneLaunch = new Phaser.Class({
@@ -82,43 +78,11 @@ function setupExplore()
 
         create: function()
         {
+            this.physics.world.gravity.y = 200;
+
             var graphics = this.add.graphics();
-            graphics.fillGradientStyle(0x000000, 0x000000, 0x0000ff, 0x0000ff);
-            graphics.fillRect(0, 0, 1000, 2500);
-
-            particlesManager = this.add.particles('fire');
-
-            rocket = this.physics.add.sprite(500, 2437.5, 'rocket');
-            rocket.setOrigin(0.5, 0.5)
-                  .setBounce(0.1)
-                  .setCollideWorldBounds(true);
-
-            zone = this.add.zone(0, 0).setSize(1000, 3);
-            this.physics.world.enable(zone);
-            zone.body.setAllowGravity(false);
-            zone.body.moves = false;
-
-            this.physics.add.overlap(rocket, zone, function()
-            {
-                this.scene.start('sceneOrbit');
-            }, null, this);
-
-            text = this.add.text(500, 2150, "Click to launch", { font: "65px Arial", fill: "#ffffff", stroke: "#000000", strokeThickness: 5, align: "center" });
-            text.setOrigin(0.5);
-            var tween = this.tweens.add({
-                targets: text,
-                alpha: 0.35,
-                ease: 'Sine',
-                duration: 750,
-                yoyo: true,
-                repeat: -1
-            });
-
-            this.input.on('pointerdown', function()
-            {
-                rocket.body.setAccelerationY(-225);
-                text.destroy();
-            });
+            graphics.fillGradientStyle(0x000000, 0x000000, 0x83EAFF, 0x83EAFF);
+            graphics.fillRect(0, 0, 1000, 3000);
 
             var particleConfig = {
                 on: false,
@@ -133,30 +97,63 @@ function setupExplore()
                 frequency: 100,
                 maxParticles: 35
             }
-            particlesEmitterLeft = particlesManager.createEmitter(particleConfig);
-            particlesEmitterRight = particlesManager.createEmitter(particleConfig);
+            particlesManager = this.add.particles('fire');
+            this.particlesEmitterLeft = particlesManager.createEmitter(particleConfig);
+            this.particlesEmitterRight = particlesManager.createEmitter(particleConfig);
 
-            this.physics.world.setBounds(0, 0, 1000, 2500);
+            this.rocket = this.physics.add.sprite(500, 2937.5, 'rocket');
+            this.rocket.setOrigin(0.5, 0.5)
+                       .setBounce(0.1)
+                       .setCollideWorldBounds(true);
 
-            this.cameras.main.setBounds(0, 300, 1000, 2200);
-            this.cameras.main.startFollow(rocket);
-            particlesEmitterLeft.startFollow(rocket, -rocket.width / 4, rocket.height / 2);
-            particlesEmitterRight.startFollow(rocket, rocket.width / 4, rocket.height / 2);
+            this.particlesEmitterLeft.startFollow(this.rocket, -this.rocket.width / 4, this.rocket.height / 1.75);
+            this.particlesEmitterRight.startFollow(this.rocket, this.rocket.width / 4, this.rocket.height / 1.75);
+            this.cameras.main.startFollow(this.rocket);
+
+            zone = this.add.zone(0, 0).setSize(1000, 3);
+            this.physics.world.enable(zone);
+            zone.body.setAllowGravity(false);
+            zone.body.moves = false;
+
+            this.physics.add.overlap(this.rocket, zone, function()
+            {
+                this.scene.start('sceneOrbit');
+            }, null, this);
+
+            text = this.add.text(500, 2650, "Click to launch", { font: "65px Arial", fill: "#ffffff", stroke: "#000000", strokeThickness: 5, align: "center" });
+            text.setOrigin(0.5);
+            this.tweens.add({
+                targets: text,
+                alpha: 0.35,
+                ease: 'Sine',
+                duration: 750,
+                yoyo: true,
+                repeat: -1
+            });
+
+            this.input.on('pointerdown', function()
+            {
+                this.rocket.body.setAccelerationY(-225);
+                text.destroy();
+            }.bind(this));
+
+            this.physics.world.setBounds(0, 0, 1000, 3000);
+            this.cameras.main.setBounds(0, 500, 1000, 2500);
         },
 
         update: function()
         {
-            if (rocket.body.acceleration.y < 0)
+            if (this.rocket.body.acceleration.y < 0)
             {
-                var ratio = rocket.y / 2500;
+                var ratio = this.rocket.y / 3500;
                 this.cameras.main.shake(100, 0.0025 * ratio);
-                particlesEmitterLeft.start();
-                particlesEmitterRight.start();
+                this.particlesEmitterLeft.start();
+                this.particlesEmitterRight.start();
             }
             else
             {
-                particlesEmitterLeft.stop();
-                particlesEmitterRight.stop();
+                this.particlesEmitterLeft.stop();
+                this.particlesEmitterRight.stop();
             }
         }
     });
@@ -178,73 +175,35 @@ function setupExplore()
 
         create: function()
         {
+            this.physics.world.gravity.y = 0;
+
             var graphics = this.add.graphics();
-            graphics.fillGradientStyle(0x000000, 0x000000, 0x0000ff, 0x0000ff);
-            graphics.fillRect(0, 0, 1000, 2500);
+            graphics.fillGradientStyle(0x000000, 0x000000, 0x333333, 0x333333);
+            graphics.fillRect(0, 0, 1000, 680);
 
-            particlesManager = this.add.particles('fire');
+            this.rocket = this.physics.add.sprite(500, 340, 'rocket');
+            this.rocket.setOrigin(0.5, 0.5);
 
-            rocket = this.physics.add.sprite(500, 2437.5, 'rocket');
-            rocket.setOrigin(0.5, 0.5)
-                  .setBounce(0.1)
-                  .setCollideWorldBounds(true);
-
-            text = this.add.text(500, 2150, "Click to launch", { font: "65px Arial", fill: "#ffffff", stroke: "#000000", strokeThickness: 5, align: "center" });
-            text.setOrigin(0.5);
-
-            var tween = this.tweens.add({
-                targets: text,
-                alpha: 0.35,
-                ease: 'Sine',
-                duration: 750,
-                yoyo: true,
+            this.orbitCircle = new Phaser.Geom.Circle(500, 320, 250);
+            this.orbitRotation = this.tweens.addCounter({
+                from: 0,
+                to: 6.28,
+                duration: 20000,
                 repeat: -1
-            });
+            })
 
-            this.input.on('pointerdown', function()
-            {
-                rocket.body.setAccelerationY(-225);
-                text.destroy();
-            });
-
-            var particleConfig = {
-                on: false,
-                alpha: { start: 1, end: 0 },
-                scale: { start: 0.65, end: 1.5 },
-                accelerationY: 225,
-                gravityY: 200,
-                angle: { min: -85, max: -95 },
-                rotate: { min: -180, max: 180 },
-                lifespan: { min: 500, max: 600 },
-                blendMode: 'ADD',
-                frequency: 100,
-                maxParticles: 35
-            }
-            particlesEmitterLeft = particlesManager.createEmitter(particleConfig);
-            particlesEmitterRight = particlesManager.createEmitter(particleConfig);
-
-            this.physics.world.setBounds(0, 0, 1000, 2500);
-
-            this.cameras.main.setBounds(0, 300, 1000, 2200);
-            this.cameras.main.startFollow(rocket);
-            particlesEmitterLeft.startFollow(rocket, -rocket.width / 4, rocket.height / 2);
-            particlesEmitterRight.startFollow(rocket, rocket.width / 4, rocket.height / 2);
+            this.physics.world.setBounds(0, 0, 1000, 680);
+            this.cameras.main.setBounds(0, 0, 1000, 680);
         },
 
         update: function()
         {
-            if (rocket.body.acceleration.y < 0)
-            {
-                var ratio = rocket.y / 2500;
-                this.cameras.main.shake(100, 0.0025 * ratio);
-                particlesEmitterLeft.start();
-                particlesEmitterRight.start();
-            }
-            else
-            {
-                particlesEmitterLeft.stop();
-                particlesEmitterRight.stop();
-            }
+            Phaser.Actions.PlaceOnCircle(
+                [this.rocket],
+                this.orbitCircle,
+                this.orbitRotation.getValue()
+            );
+            this.rocket.rotation = this.orbitRotation.getValue() + 3.14;
         }
     });
 
@@ -255,7 +214,6 @@ function setupExplore()
         physics: {
             default: 'arcade',
             arcade: {
-                gravity: { y: 200 },
                 debug: true
             }
         },
