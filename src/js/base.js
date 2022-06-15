1779cdca841cdf7f603a727b2e01ad9bea3fa978
Kyle Bignell <Kyle.Bignell@gmail.com>
2022-06-15 21:58:33 +0100

    Updated experience and skills

--- src/js/base.js
+++
@@ -1,5 +0,0 @@
-window.addEventListener('load', function() {
-  var elems = window.document.querySelectorAll('.sidenav');
-  var options = {};
-  var instances = M.Sidenav.init(elems, options);
-});
